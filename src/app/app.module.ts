b6301c465554585d4d7b12cb79017be743f05693
Evan Chesterman <chestermane@gmail.com>
2020-10-16 13:03:03 -0400

    Space X app initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import {SpacexService} from './services/spacex.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import { MissionsComponent } from './missions/missions.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AllFlightsComponent } from './all-flights/all-flights.component';
-import { AboutComponent } from './about/about.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { FlightDetailsComponent } from './flight-details/flight-details.component';
-import { SanitizePipe } from '../pipes/sanitize.pipe';
-
-
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { SpacexService } from "./services/spacex.service";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MissionsComponent } from "./missions/missions.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { AllFlightsComponent } from "./all-flights/all-flights.component";
+import { AboutComponent } from "./about/about.component";
+import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { FlightDetailsComponent } from "./flight-details/flight-details.component";
+import { SanitizePipe } from "../pipes/sanitize.pipe";
 
 @NgModule({
   declarations: [
@@ -30,16 +27,15 @@ import { SanitizePipe } from '../pipes/sanitize.pipe';
     AllFlightsComponent,
     AboutComponent,
     FlightDetailsComponent,
-    SanitizePipe
+    SanitizePipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     InfiniteScrollModule,
-  
   ],
   providers: [SpacexService, SanitizePipe],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
